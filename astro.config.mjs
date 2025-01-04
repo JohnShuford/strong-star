9edeab8a9ce892d06b15ef1c386ca5c0f2980ba2
John Shuford <johnshuford@me.com>
2025-01-04 00:04:47 -0700

    Fixing the svelte error. I needed to download the package into my computer!

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react(), tailwind()],
+    site: 'https://johnshuford.net/',
+    integrations: [mdx(), sitemap(), react(), tailwind(), svelte()],
 });
